6db941dfe8c76352da10711fe692d45c36c00a1c
Khosraw <khosraw.azizi@gmail.com>
2021-07-16 16:28:42 -0400

    added a bit more privacy with the password hiding itself

--- src/me/khosraw/Main.java
+++ src/me/khosraw/Main.java
@@ -15,20 +15,34 @@ public class Main {
         properties.put("mail.smtp.port", "587");
 
         JTextField value1 = new JTextField();
-        JTextField value2 = new JTextField();
+
 
         String email = null;
         String password = null;
 
         Object[] messagesCred = {
                 "Email Address: ", value1,
-                "Password: ", value2,
         };
-
         int login = JOptionPane.showConfirmDialog(null, messagesCred, "Enter all your credentials!", JOptionPane.OK_CANCEL_OPTION);
         if (login == JOptionPane.OK_OPTION) {
             email = value1.getText();
-            password = value2.getText();
+        }
+
+        JPanel panel = new JPanel();
+        JLabel label = new JLabel("Enter a password:");
+        JPasswordField pass = new JPasswordField(16);
+        panel.add(label);
+        panel.add(pass);
+        String[] options1 = new String[]{"OK", "Cancel"};
+        int option1 = JOptionPane.showOptionDialog(null, panel, "The title",
+                JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE,
+                null, options1, options1[1]);
+        if(option1 == 0) // pressing OK button
+        {
+            char[] passwordChar = pass.getPassword();
+            password = new String(passwordChar);
+        } else {
+            JOptionPane.showMessageDialog(null, "Looks like you exited the program. If you think this is a mistake, please report it to the developer!");
         }
 
         JTextField value3 = new JTextField();
@@ -69,9 +83,9 @@ public class Main {
             Transport.send(message);
             JOptionPane.showMessageDialog(null, "Email successfully sent!");
 
-            String[] options = {"Yes", "No"};
-            int option = JOptionPane.showOptionDialog(null, "Would you like to send more emails?","Continue?", JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, options, options[0]);
-            switch (option) {
+            String[] options2 = {"Yes", "No"};
+            int option2 = JOptionPane.showOptionDialog(null, "Would you like to send more emails?","Continue?", JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, options2, options2[0]);
+            switch (option2) {
                 case 0:
                     break;
                 case 1:

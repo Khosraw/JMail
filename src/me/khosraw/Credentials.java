d6af15b3988e05ecfc0e19747c69590542a8bdad
Khosraw <khosraw.azizi@gmail.com>
2021-07-17 11:12:05 -0400

    added SHA hashing algorithm for passwords when needed to output

--- src/me/khosraw/Credentials.java
+++ src/me/khosraw/Credentials.java
@@ -1,5 +1,9 @@
 package me.khosraw;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class Credentials {
     String email;
     String password;
@@ -13,6 +17,25 @@ public class Credentials {
         this.to = to;
         this.subject = subject;
         this.content = content;
+    }
+
+    public static String credentials (String email, String password, String to, String subject, String content) {
+        StringBuilder sb = new StringBuilder();
+
+        try {
+            MessageDigest messageDigest = MessageDigest.getInstance("SHA");
+
+            messageDigest.update(password.getBytes(StandardCharsets.UTF_8));
+
+            byte[] resultByArray = messageDigest.digest();
+
+            for (byte b : resultByArray) {
+                sb.append(String.format("%02x", b));
+            }
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return email + "\n" + sb.toString() + "\n" + to + "\n" + subject + "\n" + content;
     }
 
     public String getEmail () {
